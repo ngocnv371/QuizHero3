651f790096f4c79313d3286b938f01ecc00000d3
Ngoc Ngo <ngocnv371@gmail.com>
2025-01-10 20:13:22 -0800

    add azure terraform

---
+++ etc/azure/main.tf
@@ -0,0 +1,17 @@
+# helper to generate random names for resources
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "quizhero"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
