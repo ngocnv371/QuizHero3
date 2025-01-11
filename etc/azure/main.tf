acd9b370dd82b692d7b742c101a7397e4cb31c1d
Ngoc Ngo <ngocnv371@gmail.com>
2025-01-11 00:49:39 -0800

    auto set conn string

--- etc/azure/main.tf
+++ etc/azure/main.tf
@@ -40,6 +40,42 @@ resource "azurerm_mssql_database" "db" {
   server_id = azurerm_mssql_server.server.id
 }
 
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = random_pet.rg_name.id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = random_pet.rg_name.id
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  depends_on            = [azurerm_service_plan.appserviceplan]
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+  }
+  connection_string {
+    name  = "default"
+    type  = "SQLServer"
+    value = "Server=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${var.sql_db_name};Persist Security Info=False;User ID=${var.admin_username};Password=${local.admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+}
+
+#  Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/ngocnv371/QuizHero3"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
+}
+
 resource "azurerm_container_registry" "acr" {
   name                = "quizhero"
   resource_group_name = azurerm_resource_group.rg.name
