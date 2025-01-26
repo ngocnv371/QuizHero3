bfb84137317836901366f5acde0ec0700098e4bd
Ngoc Ngo <ngocnv371@gmail.com>
2025-01-26 10:54:54 +0700

    add pristine check

--- zalo/src/modules/profile/components/location-form.tsx
+++ zalo/src/modules/profile/components/location-form.tsx
@@ -6,20 +6,26 @@ import { useProfile } from '@/modules/auth/use-auth'
 
 export const LocationForm: React.FC = () => {
   const { profile: user, actions } = useProfile()
+  const [isPristine, setIsPristine] = useState(true)
   const { openSnackbar } = useSnackbar()
   const [city, setCity] = useState('')
   const [province, setProvince] = useState('')
 
-  console.log('u', user)
-
   useEffect(() => {
     setCity(user?.extraProperties.city)
     setProvince(user?.extraProperties.province)
+    setIsPristine(true)
   }, [user])
 
   const handleCityChanged = useCallback((value: string) => {
     setCity(value)
     setProvince('')
+    setIsPristine(false)
+  }, [])
+
+  const handleProvinceChanged = useCallback((value: string) => {
+    setProvince(value)
+    setIsPristine(false)
   }, [])
 
   const handleSave = useCallback(async () => {
@@ -30,6 +36,7 @@ export const LocationForm: React.FC = () => {
         duration: 3000,
         type: 'success',
       })
+      setIsPristine(true)
     } catch {
       openSnackbar({
         text: 'Failed to update location',
@@ -42,8 +49,8 @@ export const LocationForm: React.FC = () => {
     <Box p={4}>
       <p>Lựa chọn địa điểm sẽ sắp xếp bạn vào bảng xếp hạng của khu vực đó.</p>
       <CityPicker value={city} onChange={handleCityChanged} />
-      {city && <ProvincePicker parent={city} value={province} onChange={setProvince} />}
-      <Button className="my-4" disabled={!city || !province} onClick={handleSave}>
+      {city && <ProvincePicker parent={city} value={province} onChange={handleProvinceChanged} />}
+      <Button className="my-4" disabled={!city || !province || isPristine} onClick={handleSave}>
         <Icon icon="zi-save-to-collection" />
         Save
       </Button>
