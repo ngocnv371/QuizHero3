c8d27f10ce1f6070fc9bd7f358db68530d60ceca
Ngoc Ngo <ngocnv371@gmail.com>
2025-01-27 20:48:54 +0700

    seed quiz

--- src/QuizHero.Domain/Quiz/QuizSeed.cs
+++ src/QuizHero.Domain/Quiz/QuizSeed.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Repositories;
@@ -18,82 +21,63 @@ namespace QuizHero.Quiz
 		{
 			if (await topicRepository.GetCountAsync() == 0)
 			{
-				// create topic 1
-				var topic1 = new Topic(
-					guidGenerator.Create(),
-					"General Knowledge",
-					"General",
-					"General knowledge questions",
-					"https://picsum.photos/128/128?id=1",
-					"https://picsum.photos/300/200?id=1"
-					);
-				await topicRepository.InsertAsync(topic1, true);
+				var topic1 = CreateTopic("Math", "Academic");
+				var topic2 = CreateTopic("Common", "General");
+				var topic3 = CreateTopic("Names", "General");
+				Topic[] topics = [topic1, topic2, topic3];
+				await topicRepository.InsertManyAsync(topics, true);
 
-				// create topic 2
-				var topic2 = new Topic(
-					guidGenerator.Create(),
-					"Common Sense",
-					"General",
-					"People need to know this",
-					"https://picsum.photos/128/128?id=2",
-					"https://picsum.photos/300/200?id=2"
-					);
-				await topicRepository.InsertAsync(topic2, true);
-
-				// create quiz 1
-				await SeedQuiz(topic1);
-				await SeedQuiz2(topic2);
+				for (var i = 0; i < topics.Length; i++)
+				{
+					var num = RandomHelper.GetRandom(2, 8);
+					for (var j = 0; j < num; j++)
+					{
+						await SeedQuiz(topics[i].Id);
+					}
+				}
 			}
 		}
 
-		private async Task SeedQuiz(Topic topic)
+		private Topic CreateTopic(string name, string category)
 		{
-			var quiz = new Quiz(guidGenerator.Create(), topic.Id, "Quiz 1", "Sample quiz");
+			var topic = new Topic(
+				guidGenerator.Create(),
+				name,
+				category,
+				$"Description of {name}",
+				$"https://picsum.photos/128/128?id={name}",
+				$"https://picsum.photos/300/200?id={name}"
+			);
+			return topic;
+		}
+
+		private async Task SeedQuiz(Guid topicId)
+		{
+			var index = RandomHelper.GetRandom(5000, 20_000);
+			var quiz = new Quiz(guidGenerator.Create(), topicId, $"Quiz {index}", "Sample quiz");
 			await quizRepository.InsertAsync(quiz, true);
 
-			await SeedQuestions(quiz);
+			var num = RandomHelper.GetRandom(3, 5);
+			var list = new List<Question>();
+			for (var i = 0; i < num; i++)
+			{
+				list.Add(CreateQuestion(quiz.Id));
+			}
+			await questionRepository.InsertManyAsync(list, true);
 		}
 
-		private async Task SeedQuestions(Quiz quiz)
+		private Question CreateQuestion(Guid quizId)
 		{
-			var question = new Question(guidGenerator.Create(), quiz.Id, "What is the color of the sky?");
-			question.AddAnswer(guidGenerator.Create(), "Blue", true);
-			question.AddAnswer(guidGenerator.Create(), "Green", false);
-			question.AddAnswer(guidGenerator.Create(), "Red", false);
-			question.AddAnswer(guidGenerator.Create(), "Yellow", false);
-			await questionRepository.InsertAsync(question, true);
-
-			var question2 = new Question(guidGenerator.Create(), quiz.Id, "How long is an hour?");
-			question2.AddAnswer(guidGenerator.Create(), "24 seconds", false);
-			question2.AddAnswer(guidGenerator.Create(), "60 seconds", true);
-			question2.AddAnswer(guidGenerator.Create(), "12 days", false);
-			question2.AddAnswer(guidGenerator.Create(), "yesterday", false);
-			await questionRepository.InsertAsync(question2, true);
-		}
-
-		private async Task SeedQuiz2(Topic topic)
-		{
-			var quiz = new Quiz(guidGenerator.Create(), topic.Id, "Quiz 2", "Sample quiz 2");
-			await quizRepository.InsertAsync(quiz, true);
-
-			await SeedQuestions2(quiz);
-		}
-
-		private async Task SeedQuestions2(Quiz quiz)
-		{
-			var question = new Question(guidGenerator.Create(), quiz.Id, "How long can a dog typically live?");
-			question.AddAnswer(guidGenerator.Create(), "a life", false);
-			question.AddAnswer(guidGenerator.Create(), "100 years", false);
-			question.AddAnswer(guidGenerator.Create(), "20 years", true);
-			question.AddAnswer(guidGenerator.Create(), "long", false);
-			await questionRepository.InsertAsync(question, true);
-
-			var question2 = new Question(guidGenerator.Create(), quiz.Id, "What is 1 + 3?");
-			question2.AddAnswer(guidGenerator.Create(), "3", false);
-			question2.AddAnswer(guidGenerator.Create(), "4", true);
-			question2.AddAnswer(guidGenerator.Create(), "true", false);
-			question2.AddAnswer(guidGenerator.Create(), "false", false);
-			await questionRepository.InsertAsync(question2, true);
+			var num = RandomHelper.GetRandom(1, 1_000_000);
+			var question = new Question(guidGenerator.Create(), quizId, $"Question content {num}?");
+			question.AddAnswer(guidGenerator.Create(), "Option 1", false);
+			question.AddAnswer(guidGenerator.Create(), "Option 2", false);
+			question.AddAnswer(guidGenerator.Create(), "Option 3", false);
+			question.AddAnswer(guidGenerator.Create(), "Option 4", false);
+			var correctChoice = RandomHelper.GetRandomOfList(question.Answers);
+			correctChoice.ChangeIsCorrect(true);
+			correctChoice.ChangeText(correctChoice.Text + " (correct)");
+			return question;
 		}
 	}
 }
